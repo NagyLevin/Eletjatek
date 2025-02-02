b9cb2f03dd88c7fe1efb7baed8cf205a8ec762ca
NagyLevin <levsza13@gmail.com>
2025-02-02 03:06:53 +0100

    Pont elmegy a kepernyő szélére, és közben szint vált

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class Main {
-    private static final int XX = 900;     //golbális méretek
-    private static final int YY = 600;
-
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Eletjatek");
+            JFrame frame = new JFrame("Black Window");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(XX, YY);
-            frame.setLocationRelativeTo(null); //ablak középre
+            frame.setSize(800, 600);
+            frame.setLocationRelativeTo(null);
 
-            JPanel panel = new JPanel();
-            panel.setBackground(Color.BLACK); //hatter fekete
+            MovingDotPanel panel = new MovingDotPanel();
             frame.add(panel);
 
             frame.addKeyListener(new KeyAdapter() {
@@ -31,4 +29,43 @@ public class Main {
             frame.setVisible(true);
         });
     }
+}
+
+class MovingDotPanel extends JPanel implements ActionListener {
+    private int x = 400;
+    private final int y = 300;
+    private final int dotSize = 10;
+    private final Timer timer;
+    private Color dotColor = Color.RED;
+    private final Random random;
+
+    public MovingDotPanel() {
+        setBackground(Color.BLACK);
+        timer = new Timer(50, this);
+        timer.start();
+        random = new Random();
+        dotColor = getRandomColor();
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(dotColor);
+        g.fillOval(x, y, dotSize, dotSize);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        x -= 5;
+
+        if (x < 0) x = getWidth(); //ha eleri a szelet akkoe visszakerül az emeletre
+        dotColor = getRandomColor();
+        repaint(); //refresh
+    }
+
+    private Color getRandomColor() {
+        return new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+    }
+
 }
